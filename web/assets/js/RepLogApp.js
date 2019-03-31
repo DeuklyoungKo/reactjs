1dcb021f552ce7a4b138b8abeb90187d391e986d
DeuklyoungKo <deutschgon@gmail.com>
2019-03-31 17:52:48 +0200

    finished chapter 1-17 (https://symfonycasts.com/tracks/javascript)

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -2,42 +2,23 @@
 
 (function (window, $) {
 
-    window.RepLogApp = {
+    window.RepLogApp = function($wrapper) {
+        this.$wrapper = $wrapper;
+        this.helper = new Helper($wrapper);
 
-        initialize: function($wrapper) {
-            this.$wrapper = $wrapper;
+        this.$wrapper.find('.js-delete-rep-log').on(
+            'click',
+            this.handleRepLogDelete.bind(this)
+        );
 
-            // Helper.initialize($wrapper);
-            this.helper = new Helper($wrapper);
+        this.$wrapper.find('tbody tr').on(
+            'click',
+            this.handleRowClick.bind(this)
+        );
 
-            var helper2 = new Helper($('footer'));
+    };
 
-            console.log(
-                this.helper.calculateTotalWeight(),
-                helper2.calculateTotalWeight()
-            );
-
-            // console.log(
-            //     'foo'.__proto__,
-            //     [].__proto__,
-            //     (new Date()).__proto__
-            // );
-
-            this.$wrapper.find('.js-delete-rep-log').on(
-                'click',
-                this.handleRepLogDelete.bind(this)
-            );
-
-            this.$wrapper.find('tbody tr').on(
-                'click',
-                this.handleRowClick.bind(this)
-            );
-
-        },
-
-        // whatIsThis: function(greeting) {
-        //   console.log(this, greeting);
-        // },
+    $.extend(window.RepLogApp.prototype, {
 
         updateTotalWeightLifted: function() {
             this.$wrapper.find('.js-total-weight').html(
@@ -80,16 +61,7 @@
             console.log('row clicked');
         }
 
-        // _calculateTotalWeight: function () {
-        //     var totalWeight = 0;
-        //
-        //     this.$wrapper.find('tbody tr').each(function() {
-        //         totalWeight += $(this).data('weight');
-        //     });
-        //
-        //     return  totalWeight;
-        // }
-    };
+    });
 
 
     /**
