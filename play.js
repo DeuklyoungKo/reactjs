a6f787377c31ad7c5c6599ca4b6f2685590c2302
DeuklyoungKo <deutschgon@gmail.com>
2019-05-10 10:09:26 +0200

    finished chapter 1-12 https://symfonycasts.com/screencast/javascript-es6

--- play.js
+++ play.js
@@ -1,36 +1,16 @@
-class AGreateClass {
-    constructor(greatNumber) {
-        this.greatNumber = greatNumber;
-    }
+let printThreeThings = function (thing1, thing2, thing3) {
+    console.log(thing1, thing2, thing3);
+};
 
+// let yummyThings = ['pizza', 'gelato', 'sushi'];
+let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
+let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
 
-    returnGreatThings(){
-        return this.greatNumber;
-    }
-}
+// printThreeThings(...yummyThings);
+// let copyOfGreatThing = greatThings;
+let copyOfGreatThing = [...greatThings];
 
+copyOfGreatThing.push('summer');
 
-class AnotherGreatClass extends AGreateClass{
-
-    constructor(greatNumber,greatWord) {
-        super(greatNumber);
-        this.greatWord = greatWord;
-    }
-
-    returnGreatThings() {
-
-        let greatNumber = super.returnGreatThings();
-        // return {greatNumber: 'adventure'};
-        return [greatNumber, this.greatWord];
-
-    }
-
-}
-
-// const aGreatObject = new AGreateClass(42);
-const aGreatObject = new AnotherGreatClass(42, 'adventure');
-
-console.log(
-    // aGreatObject.returnGreatThings().greatNumber
-    aGreatObject.returnGreatThings()
-);
+console.log(greatThings);
+console.log(copyOfGreatThing);
