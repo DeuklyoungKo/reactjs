61fdca91fe6102d0826d731cde29fcaef139e0e8
DeuklyoungKo <deutschgon@gmail.com>
2019-05-12 00:44:08 +0200

    finished chapter 1-13 https://symfonycasts.com/screencast/javascript-es6

--- play.js
+++ play.js
@@ -1,16 +1,61 @@
-let printThreeThings = function (thing1, thing2, thing3) {
-    console.log(thing1, thing2, thing3);
+const data = ["vote1", "vote2", "vote1", "vote2", "vote2"];
+const reducer = (accumulator, value, index, array) => {
+
+    console.log(accumulator);
+
+    if (accumulator[value]) {
+        accumulator[value] = accumulator[value] + 1;
+    } else {
+        accumulator[value] = 1;
+    }
+    return accumulator;
+};
+// const getVote = data.reduce(reducer, {}); // { vote1: 2, vote2: 3 }
+const getVote2 = data.reduce(reducer); // "vote1"
+
+
+
+/*
+
+
+var data = [1, 2, 3, 4, 5, 6, 1];
+var reducer = (accumulator, value, index, array) => {
+
+    console.log(accumulator);
+
+    var sumOfAccAndVal = accumulator + value;
+
+    // console.log(sumOfAccAndVal);
+
+    if (index === array.length - 1) {
+        return (sumOfAccAndVal) / array.length;
+    }
+    return sumOfAccAndVal;
 };
 
-// let yummyThings = ['pizza', 'gelato', 'sushi'];
-let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
-let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
+var getMean = data.reduce(reducer, 0);
+console.log(getMean); // 3.142857142857143
 
-// printThreeThings(...yummyThings);
-// let copyOfGreatThing = greatThings;
-let copyOfGreatThing = [...greatThings];
 
-copyOfGreatThing.push('summer');
 
-console.log(greatThings);
-console.log(copyOfGreatThing);
+
+const favoriteFood = 'gelato';
+const iLoveFood = `The year is ${(new Date()).getFullYear()} and my favorite food is ${favoriteFood}`;
+console.log(iLoveFood);
+
+
+
+var votes = ["kim", "hong", "lee", "hong", "lee", "lee", "hong"];
+var reducer = function(accumulator, value, index, array) {
+    if (accumulator.hasOwnProperty(value)) {
+        accumulator[value] = accumulator[value] + 1;
+    } else {
+        accumulator[value] = 1;
+    }
+    return accumulator;
+}
+var initialValue = {};
+var result = votes.reduce(reducer, initialValue);
+console.log(result); // { kim: 1, hong: 3, lee: 3 }
+*/
+
--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -209,219 +209,22 @@
 
         _clearForm() {
             this._removeFormErrors();
-
             const $form = this.$wrapper.find(RepLogApp._selectors.newRepForm);
             $form[0].reset();
         }
 
         _addRow(repLog) {
-
             let {itemLabel, reps, id, totallyMadeUpKey = 'whatever!'} = repLog;
-            console.log(id, itemLabel, reps, totallyMadeUpKey);
-            const tplText = $('#js-rep-log-row-template').html();
-            const tpl = _.template(tplText);
-
-            const html = tpl(repLog);
-            this.$wrapper.find('tbody')
-            // .append($.parseHTML(html));
-                .append(html);
+            // console.log(id, itemLabel, reps, totallyMadeUpKey);
+            // const tplText = $('#js-rep-log-row-template').html();
+            // const tplText = rowTemplate;
+            // const tpl = _.template(tplText);
+            const html = rowTemplate(repLog);
+            this.$wrapper.find('tbody').append($.parseHTML(html));
             this.updateTotalWeightLifted();
         }
 
-
-
     }
-
-    /*
-        $.extend(window.RepLogApp.prototype, {
-
-            _selectors: {
-                newRepForm: '.js-new-rep-log-form'
-            },
-
-            _url: {
-                rep_log_list: '/reps',
-                rep_log_new: '/reps',
-            },
-
-           loadRepLogs() {
-                // var self = this;
-                $.ajax({
-                    url: this._url.rep_log_list,
-                }).then(data => {
-                    // console.log(this,self);
-                    $.each(data.items, (key, repLog) => {
-                        this._addRow(repLog);
-                    });
-                });
-            },
-
-
-            updateTotalWeightLifted() {
-                this.$wrapper.find('.js-total-weight').html(
-                    // this.helper.calculateTotalWeight();
-                    this.helper.getTotalWeightString()
-                );
-            },
-
-
-            handleRepLogDelete(e) {
-                e.preventDefault();
-
-                const $link = $(e.currentTarget);
-                // var self = this;
-                Swal.fire({
-                    title: 'Delete this log?',
-                    text: "What? Did you not actually left this?",
-                    showCancelButton: true,
-                    showLoaderOnConfirm: true,
-                    preConfirm: () => this._deleteRepLog($link),
-                }).then(result => {
-                    if (result.value) {
-                        // self._deleteRepLog($link);
-                    }else if (
-                        // Read more about handling dismissals
-                        result.dismiss === Swal.DismissReason.cancel
-                    ) {
-                        console.log("canceled");
-                    }
-                }).catch(function (arg) {
-                    console.log('canceled', arg);
-                });
-            },
-
-            _deleteRepLog($link) {
-
-                $link.addClass('text-danger');
-                $link.find('.fa')
-                    .removeClass('fa-trash')
-                    .addClass('fa-spinner')
-                    .addClass('fa-spin');
-
-                const deleteUrl = $link.data('url');
-                const $row = $link.closest('tr');
-                // var self = this;
-
-                return $.ajax({
-                    url: deleteUrl,
-                    method: 'DELETE',
-                }).then(() => {
-                    $row.fadeOut('normal', () => {
-                        // $(this).remove();
-                        $row.remove();
-                        // self.updateTotalWeightLifted();
-                        this.updateTotalWeightLifted();
-                    });
-                });
-            },
-
-            handleRowClick() {
-                console.log('row clicked');
-            },
-
-            handleNewFormSubmit(e) {
-                e.preventDefault();
-
-                const $form = $(e.currentTarget);
-                const formData = {};
-                $.each($form.serializeArray(), (key, fieldData) => {
-                    formData[fieldData.name] = fieldData.value;
-                });
-
-                // var self = this;
-
-                this._saveRepLog(formData)
-                .then(data => {
-                    // self._clearForm();
-                    // self._addRow(data)
-                    this._clearForm();
-                    this._addRow(data);
-                }).catch(errorData => {
-                    // self._mapErrorsToForm(errorData.errors);
-                    this._mapErrorsToForm(errorData.errors);
-                });
-            },
-
-            _saveRepLog(data) {
-
-                // var self = this;
-
-                return new Promise((resolve,reject) => {
-
-                    const url = this._url.rep_log_new;
-
-                    $.ajax({
-                        url,
-                        method: 'POST',
-                        data: JSON.stringify(data)
-                    }).then( (data, textStatus, jqXHR) => {
-                        $.ajax({
-                            url: jqXHR.getResponseHeader('Location'),
-                        }).then((data) => {
-                            resolve(data);
-                        });
-                    }).catch(jqXHR => {
-                        const errorData = JSON.parse(jqXHR.responseText);
-                        reject(errorData);
-                    });
-                });
-            },
-
-            _mapErrorsToForm(errorData) {
-                // reset things
-                const $form = this.$wrapper.find(this._selectors.newRepForm);
-                this._removeFormErrors();
-
-                // console.log(errorData);
-
-                $form.find(':input').each( (index, element) => {
-                    // var fieldName = $(this).attr('name');
-                    // var $wrapper = $(this).closest('.form-group');
-                    const fieldName = $(element).attr('name');
-                    const $wrapper = $(element).closest('.form-group');
-                    if (!errorData[fieldName]) {
-                        // no error!
-                        return;
-                    }
-
-                    const $error = $('<span class="js-field-error help-block"></span>');
-                    $error.html(errorData[fieldName]);
-
-                    // console.log($error);
-                    // console.log($error[0].outerText);
-
-                    $wrapper.append($error);
-                    $wrapper.addClass('has-error');
-                });
-            },
-
-            _removeFormErrors() {
-                const $form = this.$wrapper.find(this._selectors.newRepForm);
-                $form.find('.js-field-error').remove();
-                $form.find('.form-group').removeClass('has-error');
-            },
-
-            _clearForm() {
-                this._removeFormErrors();
-
-                const $form = this.$wrapper.find(this._selectors.newRepForm);
-                $form[0].reset();
-            },
-
-            _addRow(repLog) {
-                // console.log(repLog);
-                const tplText = $('#js-rep-log-row-template').html();
-                const tpl = _.template(tplText);
-
-                const html = tpl(repLog);
-                this.$wrapper.find('tbody')
-                    // .append($.parseHTML(html));
-                    .append(html);
-                this.updateTotalWeightLifted();
-            }
-
-        });
-    */
 
     /**
      * A "private" object
@@ -470,6 +273,41 @@
         }
 
     }
+
+    function upper(template, ...expressions) {
+
+        // console.log(template);
+        // console.log(...expressions);
+
+        return template.reduce((accumulator, part, i) => {
+
+            // console.log(accumulator);
+            // console.log(part);
+            // console.log(i);
+
+            console.log(expressions[i - 1].toUpperCase);
+
+            return accumulator + (expressions[i - 1].toUpperCase ? expressions[i - 1].toUpperCase() : expressions[i - 1]) + part
+        })
+    }
+
+
+    // const rowTemplate = (repLog) => upper`
+    const rowTemplate = (repLog) => `
+            <tr data-weight="${repLog.totalWeightLifted}">
+                <td>${repLog.itemLabel}</td>
+                <td>${repLog.reps}</td>
+                <td>${repLog.totalWeightLifted}</td>
+                <td>
+                    <a href="#"
+                        class="js-delete-rep-log"
+                        data-url="<%= links._self %>"
+                    >
+                        <span class="fa fa-trash"></span>
+                    </a>
+                </td>
+            </tr>
+        `;
 
     window.RepLogApp = RepLogApp;
 })(window, jQuery, swal);
